b2fd25e8d4cfd5fe08632bba27234921ba7f4cef
“Mhaventhan” <mhaventhan@gmail.com>
2018-08-13 15:28:19 +0100

    instruction, play and score page has all been implemented and working

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 $(function() {
-    $(".instruction").hide();
+  // on launch to hide the following to prevent overlap
+    $(".instructions").hide();
     $(".play").hide();
     $(".score").hide();
-  $("button").click(function (event) {
-    if ($(this).text() == "instructions") {
+    // once button is clicked for instruction hide the main page
+  $("button").click(function () {
+    // if the button has the text instructions then hide main page and show instructions
+    if ($(this).text() == "Instructions") {
+      // to hide main menu
       $(".mainMenu").hide();
+      // to show instructions
       $(".instructions").show();
+    };
+    if ($(this).text().toLowerCase() == "play") {
+      // to hide main menu
+      $(".mainMenu").hide();
+
+      // to show instructions
+      $(".play").show();
+    }
+    if ($(this).text().toLowerCase() == "score") {
+      // to hide main menu
+      $(".mainMenu").hide();
+
+      // to show instructions
+      $(".score").show();
     }
   })
 })
