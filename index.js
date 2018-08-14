5121464f278e85596f7ea1a505794a0e88f5ead0
“Mhaventhan” <mhaventhan@gmail.com>
2018-08-14 14:15:47 +0100

    game veriables added

--- index.js
+++ index.js
@@ -42,18 +42,44 @@ $(function() {
   ball_width = parseInt(ball.width());
 
   // Game priciple variables
+  // when the game is over will set to true
   var game_over = false,
+  // to receive ball and paddle center position
   ball_center,
   paddle_center,
+  // used for the movement of the ball
   ball_go = 'down',
   ball_right_left = 'right';
+  // depends on the ball movement pixels
   top = 6,
   right_left_angle = 0,
+  // for player 1 paddle movement
   move_up_p1 = false,
   move_down_p1 = false,
+  // for player 2 paddle movement
   move_up_p2 = false,
-  move_down_p2 = false,
-  winner;
+  move_down_p2 = false;
+  var who_won;
+  // -----------------GAME FUNCTIONS START HERE---------------------------------
+
+// Collision detection algorithm
+  function collision($div1, $div2) {
+    var x1 = $div1.offset().left;
+    var y1 = $div1.offset().top;
+    var h1 = $div1.outerHeight(true);
+    var w1 = $div1.outerWidth(true);
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+    var x2 = $div2.offset().left;
+    var y2 = $div2.offset().top;
+    var h2 = $div2.outerHeight(true);
+    var w2 = $div2.outerWidth(true);
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
+
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+    return true;
+}
 
 
 
